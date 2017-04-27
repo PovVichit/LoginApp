edf4f19dec7a336991c379bc3eb443c022eff4fa
Vichit Pov <vichitpov.vichit@gmail.com>
2017-04-27 12:47:50 +0700

    Add new Java Class( ClassNews) Create getter and setter in this class

---
+++ app/src/main/java/com/example/vichit/loginapp/ClassNews.java
@@ -0,0 +1,26 @@
+package com.example.vichit.loginapp;
+
+/**
+ * Created by VICHIT on 4/27/2017.
+ */
+
+public class ClassNews {
+    String name ;
+    String age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+}
